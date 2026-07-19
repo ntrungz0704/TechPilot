fddfa85b63bc686291d4b2cad8b46b642fe33e56
YMC <minhhieu3901@gmail.com>
2026-07-20 05:56:47 +0700

    UI: Cải thiện Top Bar - Đảo chiều chữ chạy sang phải, tối ưu padding và duplicate groups thành 4 để seamless loop

--- app/views/layouts/partials/topbar.php
+++ app/views/layouts/partials/topbar.php
@@ -1,35 +1,65 @@
 <div class="techpilot-topbar">
     <div class="container techpilot-topbar__container">
         
-        <div class="techpilot-topbar__marquee">
+        <div class="techpilot-topbar__viewport">
             <div class="techpilot-topbar__track">
                 <!-- Group 1 -->
                 <div class="techpilot-topbar__group">
                     <a href="#" class="techpilot-topbar__message">
-                        <i class="fa-solid fa-shield"></i> 
+                        <i class="fa-solid fa-shield" aria-hidden="true"></i> 
                         <span class="techpilot-topbar__text">Sản phẩm chính hãng – Xuất VAT đầy đủ</span>
                     </a>
                     <a href="#" class="techpilot-topbar__message">
-                        <i class="fa-solid fa-truck-fast"></i> 
+                        <i class="fa-solid fa-truck-fast" aria-hidden="true"></i> 
                         <span class="techpilot-topbar__text">Giao nhanh – Miễn phí cho đơn từ 300K</span>
                     </a>
                     <a href="#" class="techpilot-topbar__message">
-                        <i class="fa-solid fa-rotate"></i> 
+                        <i class="fa-solid fa-rotate" aria-hidden="true"></i> 
                         <span class="techpilot-topbar__text">Thu cũ giá tốt – Lên đời tiết kiệm</span>
                     </a>
                 </div>
-                <!-- Group 2 (Clone for infinite animation) -->
+                <!-- Group 2 (Clone 1) -->
                 <div class="techpilot-topbar__group" aria-hidden="true">
                     <a href="#" class="techpilot-topbar__message" tabindex="-1">
-                        <i class="fa-solid fa-shield"></i> 
+                        <i class="fa-solid fa-shield" aria-hidden="true"></i> 
                         <span class="techpilot-topbar__text">Sản phẩm chính hãng – Xuất VAT đầy đủ</span>
                     </a>
                     <a href="#" class="techpilot-topbar__message" tabindex="-1">
-                        <i class="fa-solid fa-truck-fast"></i> 
+                        <i class="fa-solid fa-truck-fast" aria-hidden="true"></i> 
                         <span class="techpilot-topbar__text">Giao nhanh – Miễn phí cho đơn từ 300K</span>
                     </a>
                     <a href="#" class="techpilot-topbar__message" tabindex="-1">
-                        <i class="fa-solid fa-rotate"></i> 
+                        <i class="fa-solid fa-rotate" aria-hidden="true"></i> 
+                        <span class="techpilot-topbar__text">Thu cũ giá tốt – Lên đời tiết kiệm</span>
+                    </a>
+                </div>
+                <!-- Group 3 (Clone 2) -->
+                <div class="techpilot-topbar__group" aria-hidden="true">
+                    <a href="#" class="techpilot-topbar__message" tabindex="-1">
+                        <i class="fa-solid fa-shield" aria-hidden="true"></i> 
+                        <span class="techpilot-topbar__text">Sản phẩm chính hãng – Xuất VAT đầy đủ</span>
+                    </a>
+                    <a href="#" class="techpilot-topbar__message" tabindex="-1">
+                        <i class="fa-solid fa-truck-fast" aria-hidden="true"></i> 
+                        <span class="techpilot-topbar__text">Giao nhanh – Miễn phí cho đơn từ 300K</span>
+                    </a>
+                    <a href="#" class="techpilot-topbar__message" tabindex="-1">
+                        <i class="fa-solid fa-rotate" aria-hidden="true"></i> 
+                        <span class="techpilot-topbar__text">Thu cũ giá tốt – Lên đời tiết kiệm</span>
+                    </a>
+                </div>
+                <!-- Group 4 (Clone 3) -->
+                <div class="techpilot-topbar__group" aria-hidden="true">
+                    <a href="#" class="techpilot-topbar__message" tabindex="-1">
+                        <i class="fa-solid fa-shield" aria-hidden="true"></i> 
+                        <span class="techpilot-topbar__text">Sản phẩm chính hãng – Xuất VAT đầy đủ</span>
+                    </a>
+                    <a href="#" class="techpilot-topbar__message" tabindex="-1">
+                        <i class="fa-solid fa-truck-fast" aria-hidden="true"></i> 
+                        <span class="techpilot-topbar__text">Giao nhanh – Miễn phí cho đơn từ 300K</span>
+                    </a>
+                    <a href="#" class="techpilot-topbar__message" tabindex="-1">
+                        <i class="fa-solid fa-rotate" aria-hidden="true"></i> 
                         <span class="techpilot-topbar__text">Thu cũ giá tốt – Lên đời tiết kiệm</span>
                     </a>
                 </div>
@@ -38,15 +68,13 @@
 
         <div class="techpilot-topbar__actions">
             <a href="<?= url('post') ?>" class="techpilot-topbar__action">
-                <i class="fa-solid fa-store"></i> <span class="techpilot-topbar__action-text">Cửa hàng gần bạn</span>
+                <i class="fa-solid fa-store" aria-hidden="true"></i> <span class="techpilot-topbar__action-text">Cửa hàng gần bạn</span>
             </a>
-            <span class="techpilot-topbar__divider">|</span>
             <a href="<?= url('profile/orders') ?>" class="techpilot-topbar__action">
-                <i class="fa-solid fa-file-invoice"></i> <span class="techpilot-topbar__action-text">Tra cứu đơn hàng</span>
+                <i class="fa-solid fa-file-invoice" aria-hidden="true"></i> <span class="techpilot-topbar__action-text">Tra cứu đơn hàng</span>
             </a>
-            <span class="techpilot-topbar__divider">|</span>
             <a href="tel:18002097" class="techpilot-topbar__action techpilot-topbar__action--hotline">
-                <i class="fa-solid fa-phone"></i> <span>1800 2097</span>
+                <i class="fa-solid fa-phone" aria-hidden="true"></i> <span>1800 2097</span>
             </a>
         </div>
         
