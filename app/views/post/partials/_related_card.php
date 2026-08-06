4723d69342a22679ff2a633753a4f36970ab15c0
Ntrungz07 <ntrungz0704@gmail.com>
2026-08-06 11:51:38 +0700

    fix(ui): fix text truncation bug in related posts sidebar widget

--- app/views/post/partials/_related_card.php
+++ app/views/post/partials/_related_card.php
@@ -5,17 +5,21 @@
  * Dùng postImageUrl() từ helpers.php
  */
 ?>
-<div class="news-related-item">
-    <a href="<?= url('post/detail/' . e($r['slug'])) ?>" class="news-related-img" aria-label="<?= e($r['title']) ?>">
+<a href="<?= url('post/detail/' . e($r['slug'])) ?>" class="news-related-item">
+    <?php if (!empty($r['image'])): ?>
+    <div class="news-related-img">
         <img
-            src="<?= postImageUrl($r['image'] ?? '') ?>"
-            alt="<?= e($r['title']) ?>"
+            src="<?= postImageUrl($r['image']) ?>"
+            alt="<?= e($r['title'] ?? '') ?>"
             loading="lazy"
             onerror="this.src='<?= url('assets/images/products/placeholder-component.webp') ?>'"
         >
-    </a>
-    <div class="news-related-content">
-        <h5><a href="<?= url('post/detail/' . e($r['slug'])) ?>"><?= e($r['title']) ?></a></h5>
-        <span><i class="fa-regular fa-clock" aria-hidden="true"></i> <?= date('d/m/Y', strtotime($r['published_at'] ?? $r['created_at'])) ?></span>
     </div>
-</div>
+    <?php endif; ?>
+    <div class="news-related-content">
+        <h5><?= e($r['title'] ?? '') ?></h5>
+        <?php if (!empty($r['published_at']) || !empty($r['created_at'])): ?>
+        <span><i class="fa-regular fa-clock" aria-hidden="true"></i> <?= date('d/m/Y', strtotime($r['published_at'] ?? $r['created_at'])) ?></span>
+        <?php endif; ?>
+    </div>
+</a>
