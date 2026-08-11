b83ed18f7bc675d15fe5a40efbff9c20139b21ba
ntrungz0704 <ntrungz0704@gmail.com>
2026-08-11 18:45:44 +0700

    fix(router): add /pc-builder route alias and cleanup one-off dev debug scripts

--- public/index.php
+++ public/index.php
@@ -311,6 +311,7 @@ $router->get('/thu-cu-doi-moi', 'HomeController@trade_in');
 
 // PC Builder Routes
 $router->get('/build-pc', 'PcBuilderController@index');
+$router->get('/pc-builder', 'PcBuilderController@index');
 $router->get('/pc-builder/products', 'PcBuilderController@getProducts');
 $router->get('/pc-builder/prebuilt', 'PcBuilderController@prebuilt');
 $router->post('/pc-builder/analysis', 'PcBuilderController@getAnalysis');
